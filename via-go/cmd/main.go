f1816c837136018be6f6f672cf3dba6c87f02a54
mike rosset <mike.rosset@gmail.com>
2011-09-09 08:09:27 -0700

    vpack: cmd add package function

--- via-go/cmd/main.go
+++ via-go/cmd/main.go
@@ -11,21 +11,26 @@ import (
 
 var (
 	arch = flag.String("arch", "x86_64", "which cpu architect to use")
-	root = flag.String("root", "./tmp", "specify the root directory")
+	root = flag.String("root", "/", "specify the root directory")
 )
-
-func init() {
-	log.SetPrefix(os.Args[0] + ": ")
-	log.SetFlags(0)
-}
 
 func main() {
 	flag.Parse()
 	args := flag.Args()
+	if len(args) == 0 {
+		flag.Usage()
+		os.Exit(2)
+	}
 	switch args[0] {
 	case "install":
 		args = shift(args)
 		err := install(args)
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "pack":
+		args = shift(args)
+		err := pack(args)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -34,29 +39,51 @@ func main() {
 	}
 }
 
-func pack(name, arch string) (err os.Error) {
-	err = via.Package(name, arch)
+func pack(targets []string) (err os.Error) {
+	for _, target := range targets {
+		log.Printf("packing %s ", target)
+		err = via.Package(target, *arch)
+		if err != nil {
+			return err
+		}
+	}
 	return
 }
 
 func install(targets []string) (err os.Error) {
+	if !fileExists(*root) {
+		err = os.MkdirAll(*root, 0755)
+		if err != nil {
+			return err
+		}
+	}
 	for _, target := range targets {
 		plan, err := via.FindPlan(target)
 		if err != nil {
 			return err
 		}
-		fmt.Printf("install %-10.10s", plan.NameVersion())
+		log.Printf("installing %s", plan.NameVersion())
 		file := fmt.Sprintf("%s-%s.tar.gz", plan.NameVersion(), *arch)
 		file = filepath.Join(via.GetRepo(), *arch, file)
 		err = via.Unpack(*root, file)
 		if err != nil {
 			return err
 		}
-		fmt.Printf("done\n")
 	}
 	return
 }
 
 func shift(a []string) []string {
 	return append(a[:0], a[0+1:]...)
+}
+
+func fileExists(path string) bool {
+	fi, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	if fi.IsRegular() || fi.IsDirectory() {
+		return true
+	}
+	return false
 }
