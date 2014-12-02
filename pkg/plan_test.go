6b29809d5c9e3e3f8e6e31a1f16b6d38b18bb706
mike rosset <mike.rosset@gmail.com>
2014-12-02 15:15:08 -0800

    use mock plan for testing

--- pkg/plan_test.go
+++ pkg/plan_test.go
@@ -5,16 +5,30 @@ import (
 )
 
 var (
-	tpp = &Plan{
-		Name:    "plan",
-		Version: "1.0",
+	testPlan = &Plan{
+		Name:         "plan",
+		Version:      "1.0",
+		Url:          "http://foo.com/plan-1.0.tar.gz",
+		BuildInStage: true,
+		Package:      []string{"cp a.out $PKGDIR/"},
+		Files:        []string{"a.out"},
 	}
 )
 
+func TestExpand(t *testing.T) {
+	var (
+		expect = "http://foo.com/plan-1.0.tar.gz"
+		got    = testPlan.GetUrl()
+	)
+	if expect != got {
+		t.Errorf("expected %s got %s", expect, got)
+	}
+}
+
 func TestBuildDir(t *testing.T) {
 	var (
-		expect = "/home/strings/via/cache/bld/plan-1.0"
-		got    = tpp.GetBuildDir()
+		expect = "/home/strings/via/cache/stg/plan-1.0"
+		got    = testPlan.GetBuildDir()
 	)
 	if got != expect {
 		t.Errorf("expect '%s' -> got '%s'", expect, got)
@@ -23,7 +37,7 @@ func TestBuildDir(t *testing.T) {
 func TestStageDir(t *testing.T) {
 	var (
 		expect = "/home/strings/via/cache/stg/plan-1.0"
-		got    = tpp.GetStageDir()
+		got    = testPlan.GetStageDir()
 	)
 	if got != expect {
 		t.Errorf("expect '%s' -> got '%s'", expect, got)
--- pkg/via_test.go
+++ pkg/via_test.go
@@ -2,27 +2,24 @@ package via
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"reflect"
 	"testing"
 )
 
 var (
-	test  = "sed"
-	repo  = "testdata/repo"
-	tplan *Plan
+	test          = "sed"
+	repo          = "testdata/repo"
+	expectDepends = []string{"glibc"}
+	expectFiles   = []string{
+		"usr/local/via/bin/sed",
+	}
 )
 
 func init() {
 	Verbose(false)
 	os.MkdirAll(repo, 0700)
 	config.Repo = repo
-	var err error
-	tplan, err = FindPlan(test)
-	if err != nil {
-		log.Fatal(err)
-	}
 }
 
 func TestLint(t *testing.T) {
@@ -38,13 +35,6 @@ func TestRepoCreate(t *testing.T) {
 	}
 }
 
-func TestBuildsteps(t *testing.T) {
-	Clean(tplan.Name)
-	if err := BuildSteps(tplan); err != nil {
-		t.Fatal(err)
-	}
-}
-
 func TestReadelf(t *testing.T) {
 	err := Readelf(join(cache.Pkgs(), "ccache-3.1.7/bin/ccache"))
 	if err != nil {
@@ -52,14 +42,13 @@ func TestReadelf(t *testing.T) {
 	}
 }
 
-var expectFiles = []string{
-	"usr/local/via/bin/sed",
-}
-
-var expectDepends = []string{"glibc"}
-
 func TestPackage(t *testing.T) {
-	pfile := tplan.PackagePath()
+	Clean(testPlan.Name)
+	os.Exit(1)
+	if err := BuildSteps(testPlan); err != nil {
+		t.Fatal(err)
+	}
+	pfile := testPlan.PackagePath()
 	got, err := ReadPackManifest(pfile)
 	if err != nil {
 		t.Error(err)
@@ -82,17 +71,6 @@ func TestRepoSync(t *testing.T) {
 	err := PlanSync()
 	if err != nil {
 		t.Error(err)
-	}
-}
-
-func TestExpand(t *testing.T) {
-	var (
-		tplan, _ = FindPlan("make")
-		expect   = "http://mirrors.kernel.org/gnu/make/make-4.1.tar.bz2"
-		got      = tplan.Expand("Url")
-	)
-	if expect != got {
-		t.Errorf("expected %s got %s", expect, got)
 	}
 }
 
