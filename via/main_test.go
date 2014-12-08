f0c9957e332516a3754a221456d1997ea986668e
mike rosset <mike.rosset@gmail.com>
2014-12-08 02:06:46 +0000

    test additional arguments

--- via/main_test.go
+++ via/main_test.go
@@ -1,18 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"testing"
 )
 
+var (
+	args = os.Args
+)
+
 func TestDiff(t *testing.T) {
-	os.Args = append(os.Args, "diff", "bash")
+	os.Args = append(args, "diff", "bash")
 	main()
 }
 
 func TestGroup(t *testing.T) {
-	os.Args = append([]string{}, "build", "devel")
-	fmt.Println(os.Args)
+	os.Args = append(args, "build", "sed")
+	main()
+}
+
+func TestList(t *testing.T) {
+	os.Args = append(args, "list", "glibc")
 	main()
 }
