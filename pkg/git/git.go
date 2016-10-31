9f0eec2c474a03a8fdbc455ab6e8df07eeef16b9
mike rosset <mike.rosset@gmail.com>
2016-10-31 05:13:11 -0700

    use go-git for cloning repositories

--- pkg/git/git.go
+++ pkg/git/git.go
@@ -3,7 +3,11 @@ package git
 import (
 	"fmt"
 	"github.com/mrosset/util/file"
+	"gopkg.in/src-d/go-git.v4"
+	"io"
 	"io/ioutil"
+	"log"
+	"os"
 	"path/filepath"
 	"strings"
 )
@@ -11,6 +15,73 @@ import (
 var (
 	join = filepath.Join
 )
+
+func init() {
+	log.SetFlags(log.Lshortfile)
+}
+
+func Clone(dir, url string) error {
+	r, err := git.NewFilesystemRepository(dir)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	err = r.Clone(&git.CloneOptions{
+		URL: url,
+	})
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// ... retrieving the branch being pointed by HEAD
+	ref, err := r.Head()
+	if err != nil {
+		return (err)
+	}
+	// ... retrieving the commit object
+	commit, err := r.Commit(ref.Hash())
+	if err != nil {
+		return (err)
+	}
+
+	// ... we get all the files from the commit
+	files, err := commit.Files()
+	if err != nil {
+		return (err)
+	}
+
+	writeFile := func(f *git.File) error {
+		abs := filepath.Join(dir, f.Name)
+		dir := filepath.Dir(abs)
+
+		os.MkdirAll(dir, 0755)
+		file, err := os.Create(abs)
+		if err != nil {
+			return err
+		}
+
+		defer file.Close()
+		r, err := f.Reader()
+		if err != nil {
+			return err
+		}
+
+		defer r.Close()
+
+		if err := file.Chmod(f.Mode); err != nil {
+			return err
+		}
+
+		_, err = io.Copy(file, r)
+		return err
+	}
+	return files.ForEach(writeFile)
+}
+
+func Name(path string) {
+}
 
 // Returns the currently checked out branch for a Git directory
 func Branch(path string) (string, error) {
--- pkg/git/git_test.go
+++ pkg/git/git_test.go
@@ -1,9 +1,23 @@
 package git
 
 import (
+	"github.com/mrosset/util/file"
 	"os"
 	"testing"
 )
+
+func TestClone(t *testing.T) {
+	var (
+		expect = "testdata/config.json"
+	)
+	defer os.RemoveAll("testdata")
+	if err := Clone("testdata", "https://github.com/mrosset/plans"); err != nil {
+		t.Error(err)
+	}
+	if !file.Exists(expect) {
+		t.Errorf("exected %s but file does not exist", expect)
+	}
+}
 
 func TestBranch(t *testing.T) {
 	var (
