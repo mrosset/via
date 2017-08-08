93053061bd36208711147260ad331180bc44edf7
mike rosset <mike.rosset@gmail.com>
2017-08-07 18:14:16 -0700

    use git PlanClone

--- pkg/git.go
+++ pkg/git.go
@@ -4,11 +4,7 @@ import (
 	"fmt"
 	"github.com/mrosset/util/file"
 	"gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/core"
-	"io"
 	"io/ioutil"
-	"log"
-	"os"
 	"path/filepath"
 	"strings"
 )
@@ -17,62 +13,10 @@ import (
 // name is the reference name to clone.
 // e.g reference name ref/heads/master
 func Clone(dir, name, url string) error {
-	r, err := git.NewFilesystemRepository(join(dir, ".git"))
-	if err != nil {
-		return err
-	}
-	err = r.Clone(&git.CloneOptions{
-		URL:           url,
-		ReferenceName: core.ReferenceName(name),
+	_, err := git.PlainClone(dir, false, &git.CloneOptions{
+		URL: url,
 	})
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-
-	// ... retrieving the branch being pointed by HEAD
-	ref, err := r.Head()
-	if err != nil {
-		return (err)
-	}
-	// ... retrieving the commit object
-	commit, err := r.Commit(ref.Hash())
-	if err != nil {
-		return (err)
-	}
-
-	// ... we get all the files from the commit
-	files, err := commit.Files()
-	if err != nil {
-		return (err)
-	}
-
-	writeFile := func(f *git.File) error {
-		abs := filepath.Join(dir, f.Name)
-		dir := filepath.Dir(abs)
-
-		os.MkdirAll(dir, 0755)
-		file, err := os.Create(abs)
-		if err != nil {
-			return err
-		}
-
-		defer file.Close()
-		r, err := f.Reader()
-		if err != nil {
-			return err
-		}
-
-		defer r.Close()
-
-		if err := file.Chmod(f.Mode); err != nil {
-			return err
-		}
-
-		_, err = io.Copy(file, r)
-		return err
-	}
-	return files.ForEach(writeFile)
+	return err
 }
 
 func Name(path string) {
@@ -82,7 +26,7 @@ func Name(path string) {
 func Branch(path string) (string, error) {
 	// path, err := filepath.Abs(path)
 	// if err != nil {
-	// 	return "", err
+	//	return "", err
 	// }
 	var (
 		head = join(path, ".git/HEAD")
